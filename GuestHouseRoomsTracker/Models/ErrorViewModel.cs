d53aba9e1f89b1f19abc117fac47f2f77c75923c
AykutYamak <aykut2006yamak@gmail.com>
2025-06-19 18:47:03 +0300

    Initial commit

---
+++ GuestHouseRoomsTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GuestHouseRoomsTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
